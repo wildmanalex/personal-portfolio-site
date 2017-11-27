627384313997d71dd4c3dff5334541327b725ce4
Alex Wildman <Alex>
2017-11-26 18:10:21 -0700

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+var myRouter = new VueRouter({
+    routes: [
+
+        {
+            path: '/work',
+
+            component: function(resolve, reject){
+				// console.log('fdsfds')
+                $.get('/html/workcopy.html', function(htmlFromServer){
+					console.log(htmlFromServer)
+                    var newComponent = {
+                        template: htmlFromServer,
+                        created: function(){ console.log('created the about component')},
+                        destroyed: function(){ console.log('destroyed the about component')}
+                    }
+					// console.log(newComponent)
+                    resolve(newComponent)
+                })
+            }
+        },
+		{
+			path: '/about',
+			component:function(resolve, reject){
+				$.get('/html/about.html', function(htmlFromServer){
+					console.log('got to about reequest')
+					var newComponent = {
+						template: htmlFromServer,
+						created: function(){ console.log('created the about component')},
+						destroyed: function(){ console.log('destroyed the about component')}
+					}
+					resolve(newComponent)
+				})
+			}
+		},
+    ]
+})
+
+
+var mainVm = new Vue({
+    el: '#app',
+    router: myRouter,
+    data: {
+
+    },
+
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('./public'))
+
+app.get('/', function(req,res){
+	// console.log('got to the server')
+	res.sendFile('./public/html/index.html', {root:'./'})
+})
+app.get('/work', function(req,res){
+	console.log('hello')
+	res.sendFile('./public/html/work.html', {root:'./'})
+
+})
+
+app.listen(8080)
